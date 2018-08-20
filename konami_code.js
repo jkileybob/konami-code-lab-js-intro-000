0e13daed237c5566af2f4e83a46a14cd89565393
Kiley Bobbitt <jkileybob@aol.com>
2018-08-20 23:31:51 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,4 +1,16 @@
-const codes = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+const codes = [
+  "ArrowUp",
+  "ArrowUp",
+  "ArrowDown",
+  "ArrowDown",
+  "ArrowLeft",
+  "ArrowRight",
+  "ArrowLeft",
+  "ArrowRight",
+  "b",
+  "a"
+];
+
 function init() {
   var index = 0;
   document.body.addEventListener('keydown',
