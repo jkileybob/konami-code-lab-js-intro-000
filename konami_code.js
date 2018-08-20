232f1b62c1c114ab5844be46ef290f4bc5a530cf
Kiley Bobbitt <jkileybob@aol.com>
2018-08-20 23:24:16 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,16 +1,20 @@
-const codes = [
-  "ArrowUp",
-  "ArrowUp",
-  "ArrowDown",
-  "ArrowDown",
-  "ArrowLeft",
-  "ArrowRight",
-  "ArrowLeft",
-  "ArrowRight",
-  "b",
-  "a"
-];
-
+const codes = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 function init() {
-  // your code here
+  var index = 0;
+  document.body.addEventListener('keydown',
+  function onKeyDownHandler(e) {
+    const key = e.key;
+    if (key === codes[index]) {
+      index++;
+        console.log(index);
+        console.log(key);
+      if (index === codes.length) {
+        window.alert("Konami Code Achieved");
+          index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  });
 }
+  // your code here
